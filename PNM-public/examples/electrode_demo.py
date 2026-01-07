d1f2f2cce43800e06e97392135e797aafb017126
phibronotchi-beep <phibronotchi@gmail.com>
2026-01-06 18:21:31 -0700

    Add electrode_demo.py for visualizing electrode array

---
+++ PNM-public/examples/electrode_demo.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+from src.pnm_public.spiral_utils import toy_electrode_array
+
+def main():
+    electrodes = toy_electrode_array(n_electrodes=121, spacing_scale=0.05)
+    
+    fig, ax = plt.subplots(figsize=(8, 8))
+    ax.scatter(electrodes[:, 0], electrodes[:, 1], s=50, c='blue', edgecolors='black')
+    ax.set_aspect("equal")
+    ax.set_title("Toy Phyllotactic Electrode Array Demo")
+    ax.axis("off")
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == "__main__":
+    main()
